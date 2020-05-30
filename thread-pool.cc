5590da0ec615fca3715b71ef71bd5a414a7c09d6
CS110 tools <cs110@cs.stanford.edu>
2020-05-29 20:38:59 -0700

    This project passes 4 of the 6 default sanity check cases.

--- thread-pool.cc
+++ thread-pool.cc
@@ -1,3 +1,89 @@
+#include "thread-pool.h"
+#include <algorithm>
+using namespace std;
+using develop::ThreadPool;
+
+void ThreadPool::dispatcher() {
+  while (true) {
+    lock_guard<mutex> lgThunkQueue(mThunkQueue);
+    cvThunkQueue.wait(mThunkQueue, [this]{
+      return !thunkQueue.empty() || exit;
+    });
+    if (exit) return;
+    mThunkQueue.unlock();
+    lock_guard<mutex> lgWorkerStatus(mWorkerStatus);
+    cvWorkerStatus.wait(mWorkerStatus, [this]{
+      return find(wBusy.begin(), wBusy.end(), false) != wBusy.end();
+    });
+    for (size_t workerID = 0; workerID < wBusy.size(); workerID++) {
+      if (!wBusy[workerID]) {
+        wBusy[workerID] = true;
+        mWorkerStatus.unlock();
+        cvWorkerStatus.notify_all();
+        mThunkQueue.lock();
+        wThunks[workerID] = thunkQueue.front();
+        thunkQueue.pop();
+        mThunkQueue.unlock();
+        cvThunkQueue.notify_all();
+        sWorkers[workerID].signal();
+        break;
+      }
+    }
+  }
+}
+
+void ThreadPool::worker(size_t workerID) {
+  while (true) {
+    sWorkers[workerID].wait();
+    if (exit) return;
+    wThunks[workerID]();
+    lock_guard<mutex> lgWorkerStatus(mWorkerStatus);
+    wBusy[workerID] = false;
+    mWorkerStatus.unlock();
+    cvWorkerStatus.notify_all();
+  }
+}
+
+ThreadPool::ThreadPool(size_t numThreads) : wts(numThreads), exit(false),
+  wBusy(numThreads, false), wThunks(numThreads), sWorkers(numThreads) {
+  dt = thread([this]() { dispatcher(); });
+  for (size_t workerID = 0; workerID < numThreads; workerID++) {
+    wts[workerID] = thread([this](size_t workerID) {
+      worker(workerID);
+    }, workerID);
+  }
+}
+
+void ThreadPool::schedule(const function<void(void)>& thunk) {
+  lock_guard<mutex> lgThunkQueue(mThunkQueue);
+  thunkQueue.push(thunk);
+  mThunkQueue.unlock();
+  cvThunkQueue.notify_all();
+}
+
+void ThreadPool::wait() {
+  lock_guard<mutex> lgThunkQueue(mThunkQueue);
+  cvThunkQueue.wait(mThunkQueue, [this]{ return thunkQueue.empty(); });
+  mThunkQueue.unlock();
+  lock_guard<mutex> lgWorkerStatus(mWorkerStatus);
+  cvWorkerStatus.wait(mWorkerStatus, [this]{
+    return find(wBusy.begin(), wBusy.end(), true) == wBusy.end();
+  });
+  mWorkerStatus.unlock();
+}
+
+ThreadPool::~ThreadPool() {
+  wait();
+  exit = true;
+  cvThunkQueue.notify_all();
+  cvWorkerStatus.notify_all();
+  for (semaphore& s : sWorkers) s.signal();
+  dt.join();
+  for (thread& t : wts) t.join();
+}
+
+
+
 /**
  * File: thread-pool.cc
  * --------------------
@@ -5,125 +91,125 @@
  */
 
 
-#include "thread-pool.h"
+// #include "thread-pool.h"
 
 
 
-using namespace std;
-using develop::ThreadPool;
+// using namespace std;
+// using develop::ThreadPool;
 
-/**
- * Schedules the provided thunk (which is something that can
- * be invoked as a zero-argument function without a return value)
- * to be executed by one of the ThreadPool's threads as soon as
- * all previously scheduled thunks have been handled.
- */
-ThreadPool::ThreadPool(size_t numThreads) : totalAvailableSemaphore(numThreads), workers(numThreads){
-    // launceh a single dispatcher thread
-    dt = thread([this]() {
-        dispatcher();
-    });
-    // launch a specific number of worker threads
-    for (size_t workerID = 0; workerID < numThreads; workerID++) {
-        workers[workerID].t = thread([this](size_t workerID) {
-            worker(workerID);
-        }, workerID);
-    }
+// /**
+//  * Schedules the provided thunk (which is something that can
+//  * be invoked as a zero-argument function without a return value)
+//  * to be executed by one of the ThreadPool's threads as soon as
+//  * all previously scheduled thunks have been handled.
+//  */
+// ThreadPool::ThreadPool(size_t numThreads) : totalAvailableSemaphore(numThreads), workers(numThreads){
+//     // launceh a single dispatcher thread
+//     dt = thread([this]() {
+//         dispatcher();
+//     });
+//     // launch a specific number of worker threads
+//     for (size_t workerID = 0; workerID < numThreads; workerID++) {
+//         workers[workerID].t = thread([this](size_t workerID) {
+//             worker(workerID);
+//         }, workerID);
+//     }
 
-}
+// }
 
 
-/**
- * Schedules the provided thunk (which is something that can
- * be invoked as a zero-argument function without a return value)
- * to be executed by one of the ThreadPool's threads as soon as
- * all previously scheduled thunks have been handled.
- */
-void ThreadPool::schedule(const std::function<void(void)>& thunk) {
-    thunksQueueMutex.lock();
-    thunksQueue.push(thunk);
+// /**
+//  * Schedules the provided thunk (which is something that can
+//  * be invoked as a zero-argument function without a return value)
+//  * to be executed by one of the ThreadPool's threads as soon as
+//  * all previously scheduled thunks have been handled.
+//  */
+// void ThreadPool::schedule(const std::function<void(void)>& thunk) {
+//     thunksQueueMutex.lock();
+//     thunksQueue.push(thunk);
 
-    pendingWorksMutex.lock();
-    numPendingWorks++;
-    pendingWorksMutex.unlock();
+//     pendingWorksMutex.lock();
+//     numPendingWorks++;
+//     pendingWorksMutex.unlock();
 
-    thunksQueueMutex.unlock();
-    scheduleSemaphore.signal(); // works in schdule++
-}
+//     thunksQueueMutex.unlock();
+//     scheduleSemaphore.signal(); // works in schdule++
+// }
 
-// dispatch work to available worker
-void ThreadPool::dispatcher(){
-    while (true) {
-        scheduleSemaphore.wait(); // works in schedule--
-        if (numPendingWorks == 0) return;
-        totalAvailableSemaphore.wait(); // totalAvailable--
+// // dispatch work to available worker
+// void ThreadPool::dispatcher(){
+//     while (true) {
+//         scheduleSemaphore.wait(); // works in schedule--
+//         if (numPendingWorks == 0) return;
+//         totalAvailableSemaphore.wait(); // totalAvailable--
 
-        for (size_t workerID = 0; workerID < workers.size(); workerID++) {
-            workers[workerID].m.lock();
-            if (workers[workerID].occupied) {
-                workers[workerID].m.unlock();
-            } else {
-                workers[workerID].occupied = true;
+//         for (size_t workerID = 0; workerID < workers.size(); workerID++) {
+//             workers[workerID].m.lock();
+//             if (workers[workerID].occupied) {
+//                 workers[workerID].m.unlock();
+//             } else {
+//                 workers[workerID].occupied = true;
 
-                thunksQueueMutex.lock();
-                workers[workerID].workerFunction = thunksQueue.front();
-                thunksQueue.pop();
-                thunksQueueMutex.unlock();
+//                 thunksQueueMutex.lock();
+//                 workers[workerID].workerFunction = thunksQueue.front();
+//                 thunksQueue.pop();
+//                 thunksQueueMutex.unlock();
 
-                workers[workerID].s.signal();
-                workers[workerID].m.unlock();
-                break;
-            }
+//                 workers[workerID].s.signal();
+//                 workers[workerID].m.unlock();
+//                 break;
+//             }
            
-        }
+//         }
 
-    }
-}
+//     }
+// }
 
-// excute an assigned function
-void ThreadPool::worker(size_t workerID) {
-    while (true) {
-        workers[workerID].s.wait();
-        if (numPendingWorks == 0) return;
+// // excute an assigned function
+// void ThreadPool::worker(size_t workerID) {
+//     while (true) {
+//         workers[workerID].s.wait();
+//         if (numPendingWorks == 0) return;
 
-        workers[workerID].workerFunction();
+//         workers[workerID].workerFunction();
 
-        workers[workerID].m.lock();
-        workers[workerID].occupied = false;
-        workers[workerID].m.unlock();
+//         workers[workerID].m.lock();
+//         workers[workerID].occupied = false;
+//         workers[workerID].m.unlock();
 
-        totalAvailableSemaphore.signal(); // totalAvailable++
+//         totalAvailableSemaphore.signal(); // totalAvailable++
 
-        pendingWorksMutex.lock();
-        numPendingWorks--;
-        pendingWorksMutex.unlock();
-        cv.notify_all(); // wake up wait()
-    }
-}
-/**
- * Blocks and waits until all previously scheduled thunks
- * have been executed in full.
- */
-void ThreadPool::wait() {
-    lock_guard<mutex> lg(cvMutex);
-    cv.wait(cvMutex, [this] {
-        return numPendingWorks == 0;
-    });
-}
+//         pendingWorksMutex.lock();
+//         numPendingWorks--;
+//         pendingWorksMutex.unlock();
+//         cv.notify_all(); // wake up wait()
+//     }
+// }
+// /**
+//  * Blocks and waits until all previously scheduled thunks
+//  * have been executed in full.
+//  */
+// void ThreadPool::wait() {
+//     lock_guard<mutex> lg(cvMutex);
+//     cv.wait(cvMutex, [this] {
+//         return numPendingWorks == 0;
+//     });
+// }
 
-/**
- * Destroys the ThreadPool class
- */
-ThreadPool::~ThreadPool() {
-    wait();
-    scheduleSemaphore.signal();
-    for (size_t workerID = 0; workerID < workers.size(); workerID++) {
-        workers[workerID].s.signal();
-    }
+// /**
+//  * Destroys the ThreadPool class
+//  */
+// ThreadPool::~ThreadPool() {
+//     wait();
+//     scheduleSemaphore.signal();
+//     for (size_t workerID = 0; workerID < workers.size(); workerID++) {
+//         workers[workerID].s.signal();
+//     }
 
-    dt.join();
-    for (size_t workerID = 0; workerID < workers.size(); workerID++) {
-        workers[workerID].t.join();
-    }
-}
+//     dt.join();
+//     for (size_t workerID = 0; workerID < workers.size(); workerID++) {
+//         workers[workerID].t.join();
+//     }
+// }
 
--- thread-pool.h
+++ thread-pool.h
@@ -1,28 +1,16 @@
-/**
- * File: thread-pool.h
- * -------------------
- * Exports a ThreadPool abstraction, which manages a finite pool
- * of worker threads that collaboratively work through a sequence of tasks.
- * As each task is scheduled, the ThreadPool waits for at least
- * one worker thread to be free and then assigns that task to that worker.  
- * Threads are scheduled and served in a FIFO manner, and tasks need to
- * take the form of thunks, which are zero-argument thread routines.
- */
-
 #ifndef _thread_pool_
 #define _thread_pool_
 
-#include <cstdlib>
-#include <functional>
+#include <cstddef>     // for size_t
+#include <functional>  // for the function template used in the schedule signature
+#include <thread>      // for thread
+#include <vector>      // for vector
 #include <queue>
 #include <mutex>
-#include "semaphore.h"
-#include <thread>
 #include <condition_variable>
-// place additional #include statements here
+#include <semaphore.h>
 
 namespace develop {
-
 class ThreadPool {
  public:
 
@@ -31,11 +19,6 @@ class ThreadPool {
  * number of threads.
  */
   ThreadPool(size_t numThreads);
-
-/**
- * Destroys the ThreadPool class
- */
-  ~ThreadPool();
 
 /**
  * Schedules the provided thunk (which is something that can
@@ -51,38 +34,134 @@ class ThreadPool {
  */
   void wait();
 
+/**
+ * Waits for all previously scheduled thunks to execute, and then
+ * properly brings down the ThreadPool and any resources tapped
+ * over the course of its lifetime.
+ */
+  ~ThreadPool();
+  
  private:
-  std::thread dt;
+  std::thread dt;                                   // dispatcher thread handle
+  std::vector<std::thread> wts;                     // worker thread handles
+  bool exit;                                        // exit flag
+  std::vector<bool> wBusy;                          // worker busy status
+  std::vector<std::function<void(void)>> wThunks;   // thunks for each worker
+  std::queue<std::function<void(void)>> thunkQueue; // thunk queue
+  std::mutex mThunkQueue;                           // thunk queue mutex
+  std::condition_variable_any cvThunkQueue;         // thunk queue cv
+  std::mutex mWorkerStatus;                         // wBusy mutex
+  std::condition_variable_any cvWorkerStatus;       // wBusy cv
+  std::vector<semaphore> sWorkers;                  // worker semaphore vector
 
-  int numPendingWorks;
-  std::mutex pendingWorksMutex;
+  void dispatcher();                                // dispatcher thread routine
+  void worker(size_t workerID);                     // worker thread routine
 
-  std::mutex cvMutex;
-  std::condition_variable_any cv;
-  
-  std::queue<std::function<void(void)>> thunksQueue;
-  std::mutex thunksQueueMutex;
-
-  semaphore scheduleSemaphore; // communication between schedule and dispatcher
-  semaphore totalAvailableSemaphore; // the total available number of worker
-
-
-  struct workerInfo {
-    semaphore s;
-    std::thread t;
-    std::mutex m;
-    bool occupied;
-    std::function<void(void)> workerFunction;
-  };
-  std::vector<workerInfo> workers;
-
-  void dispatcher();
-  void worker(size_t workerID);
-  
+/**
+ * ThreadPools are the type of thing that shouldn't be cloneable, since it's
+ * not clear what it means to clone a ThreadPool (should copies of all outstanding
+ * functions to be executed be copied?).
+ *
+ * In order to prevent cloning, we remove the copy constructor and the
+ * assignment operator.  By doing so, the compiler will ensure we never clone
+ * a ThreadPool.
+ */
   ThreadPool(const ThreadPool& original) = delete;
   ThreadPool& operator=(const ThreadPool& rhs) = delete;
 };
 
 #endif
-
 }
+
+
+
+
+
+/**
+ * File: thread-pool.h
+ * -------------------
+ * Exports a ThreadPool abstraction, which manages a finite pool
+ * of worker threads that collaboratively work through a sequence of tasks.
+ * As each task is scheduled, the ThreadPool waits for at least
+ * one worker thread to be free and then assigns that task to that worker.  
+ * Threads are scheduled and served in a FIFO manner, and tasks need to
+ * take the form of thunks, which are zero-argument thread routines.
+ */
+
+// #ifndef _thread_pool_
+// #define _thread_pool_
+
+// #include <cstdlib>
+// #include <functional>
+// #include <queue>
+// #include <mutex>
+// #include "semaphore.h"
+// #include <thread>
+// #include <condition_variable>
+// // place additional #include statements here
+
+// namespace develop {
+
+// class ThreadPool {
+//  public:
+
+// /**
+//  * Constructs a ThreadPool configured to spawn up to the specified
+//  * number of threads.
+//  */
+//   ThreadPool(size_t numThreads);
+
+// /**
+//  * Destroys the ThreadPool class
+//  */
+//   ~ThreadPool();
+
+// /**
+//  * Schedules the provided thunk (which is something that can
+//  * be invoked as a zero-argument function without a return value)
+//  * to be executed by one of the ThreadPool's threads as soon as
+//  * all previously scheduled thunks have been handled.
+//  */
+//   void schedule(const std::function<void(void)>& thunk);
+
+// /**
+//  * Blocks and waits until all previously scheduled thunks
+//  * have been executed in full.
+//  */
+//   void wait();
+
+//  private:
+//   std::thread dt;
+
+//   int numPendingWorks;
+//   std::mutex pendingWorksMutex;
+
+//   std::mutex cvMutex;
+//   std::condition_variable_any cv;
+  
+//   std::queue<std::function<void(void)>> thunksQueue;
+//   std::mutex thunksQueueMutex;
+
+//   semaphore scheduleSemaphore; // communication between schedule and dispatcher
+//   semaphore totalAvailableSemaphore; // the total available number of worker
+
+
+//   struct workerInfo {
+//     semaphore s;
+//     std::thread t;
+//     std::mutex m;
+//     bool occupied;
+//     std::function<void(void)> workerFunction;
+//   };
+//   std::vector<workerInfo> workers;
+
+//   void dispatcher();
+//   void worker(size_t workerID);
+  
+//   ThreadPool(const ThreadPool& original) = delete;
+//   ThreadPool& operator=(const ThreadPool& rhs) = delete;
+// };
+
+// #endif
+
+// }
